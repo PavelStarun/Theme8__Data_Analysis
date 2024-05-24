db20305fbc344c3e2cb39e24cc159441dbbae5ff
PavelStarun <starunpavelpython@gmail.com>
2024-05-24 18:28:27 +0300

    1

--- AZ02.py
+++ AZ02.py
@@ -1,10 +1,27 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
+students = {
+    'Аня': [10, 9, 10, 9, 10],
+    'Боб': [8, 7, 10, 10, 7],
+    'Чарли': [10, 9, 7, 7, 8],
+    'Лиза': [9, 8, 9, 9, 10],
+    'Настя': [8, 6, 10, 10, 9],
+    'Саша': [10, 5, 10, 10, 6],
+    'Сергей': [7, 6, 4, 4, 7],
+    'Макс': [10, 10, 10, 10, 10],
+    'Иван': [9, 9, 7, 7, 8],
+    'Станислав': [3, 4, 5, 6, 7]
+}
+
+df_students = pd.DataFrame.from_dict(students, orient='index', columns=['Matematika', 'Biologia', 'Programirovanie', 'Informatika', 'Fizika'])
+df_students.to_csv('студенты.csv', index_label='Имя')
+
+
 df = pd.read_csv('студенты.csv')
 print(df.head())
 
-subjects = ['Matematika', 'Biologia', 'Programirovanie', 'Informatika', 'fizika']
+subjects = ['Matematika', 'Biologia', 'Programirovanie', 'Informatika', 'Fizika']
 mean_scores = df[subjects].mean()
 median_scores = df[subjects].median()
 
--- AZ02_1.py
+++ AZ02_1.py
@@ -1,6 +1,18 @@
 import pandas as pd
-import Numpy as np
+import numpy as np
 
-dates = pd.date_range('20130101', periods=6)
-df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
-print(df)
+
+df = pd.read_csv('студенты.csv')
+print(df.head())
+
+Q1_math = df['Matematika'].quantile(0.25)
+Q3_math = df['Matematika'].quantile(0.75)
+
+IQR_math = Q3_math - Q1_math
+
+std_math = np.std(df['Matematika'], ddof=1)
+
+print(f"Первый квартиль: {Q1_math}")
+print(f"Третий квартиль: {Q3_math}")
+print(f"Межквартальный размах: {IQR_math}")
+print(f"Стандартное отклонение оценок: {std_math}")
